06677a42e7636d4a06b9d9b5ff2e0b5d560f90a2
GeniusTDY <GeniusTDY@users.noreply.github.com>
2026-07-15 08:40:10 +0000

    feat: 部署CTFd到仓库
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- CTFd/themes/core/assets/js/theme/styles.js
+++ CTFd/themes/core/assets/js/theme/styles.js
@@ -1,22 +1,4 @@
 export default () => {
-  document.querySelectorAll(".form-control").forEach($el => {
-    $el.addEventListener("onfocus", () => {
-      $el.classList.remove("input-filled-invalid");
-      $el.classList.add("input-filled-valid");
-    });
-
-    $el.addEventListener("onblur", () => {
-      if ($el.nodeValue === "") {
-        $el.classList.remove("input-filled-valid");
-        $el.classList.remove("input-filled-invalid");
-      }
-    });
-
-    if ($el.nodeValue) {
-      $el.classList.add("input-filled-valid");
-    }
-  });
-
   document.querySelectorAll(".page-select").forEach($el => {
     $el.addEventListener("change", e => {
       const url = new URL(window.location);
