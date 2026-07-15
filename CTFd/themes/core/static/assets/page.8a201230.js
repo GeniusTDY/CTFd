a22a16fbd08b367a26cab1f265d00b80c2456a2b
GeniusTDY <GeniusTDY@users.noreply.github.com>
2026-07-15 08:53:29 +0000

    feat: 部署CTFd到仓库
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- CTFd/themes/core/static/assets/page.8a201230.js
+++
@@ -1,1 +0,0 @@
-import{C as o,m as d}from"./index.d5e7623d.js";window.CTFd=o;window.Alpine=d;d.start();
---
+++ CTFd/themes/core/static/assets/teams_list.c74c961e.js
@@ -0,0 +1,1 @@
+import{C as o,m as d}from"./index.c005e2b2.js";window.CTFd=o;window.Alpine=d;d.start();
