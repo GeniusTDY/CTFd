41c2693ae052200fdcd036bdb653ca208285c33c
GeniusTDY <GeniusTDY@users.noreply.github.com>
2026-07-22 06:56:35 +0000

    Revert commits c4e27c3..d58bcaa
    This reverts 3 commit(s) from c4e27c3 to d58bcaa.
    
    Reverted commits:
    d58bcaa feat: 部署CTFd i18n zh-cn-setup-translation 分支
    e9ff280 Revert commits c4e27c3..c4e27c3
    c4e27c3 feat: 部署CTFd i18n zh-cn-setup-translation 分支
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- CTFd/themes/admin/assets/js/pages/statistics.js
+++ CTFd/themes/admin/assets/js/pages/statistics.js
@@ -468,11 +468,7 @@ const graph_configs = {
           data: names,
           axisLabel: {
             interval: 0,
-            rotate: window.innerWidth <= 768 ? 30 : 50,
-            width: window.innerWidth <= 768 ? 60 : 120,
-            overflow: "truncate",
-            align: "right",
-            margin: 10,
+            rotate: 50,
           },
         },
         yAxis: {
