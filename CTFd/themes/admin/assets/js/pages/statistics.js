3200caad338a7e94d7e902dd920bda1b5b69af83
GeniusTDY <GeniusTDY@users.noreply.github.com>
2026-07-22 06:41:36 +0000

    Revert commits 8d1e117..8d53523
    This reverts 5 commit(s) from 8d1e117 to 8d53523.
    
    Reverted commits:
    8d53523 feat: 部署CTFd i18n zh-cn-setup-translation 分支
    a7026d5 Revert commits 8d1e117..bdcde3b
    bdcde3b feat: 部署CTFd i18n zh-cn-setup-translation 分支
    666dec2 Revert commits 8d1e117..8d1e117
    8d1e117 feat: 部署CTFd i18n zh-cn-setup-translation 分支
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- CTFd/themes/admin/assets/js/pages/statistics.js
+++ CTFd/themes/admin/assets/js/pages/statistics.js
@@ -442,12 +442,6 @@ const graph_configs = {
           left: "center",
           text: _("Solve Percentages per Challenge"),
         },
-        grid: {
-          left: "10%",
-          right: "10%",
-          bottom: "25%",
-          containLabel: true,
-        },
         tooltip: {
           trigger: "item",
           formatter: function (data) {
@@ -475,9 +469,6 @@ const graph_configs = {
           axisLabel: {
             interval: 0,
             rotate: 50,
-            width: 80,
-            overflow: "truncate",
-            ellipsis: "...",
           },
         },
         yAxis: {
