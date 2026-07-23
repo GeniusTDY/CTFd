86f5af34757b9a2b828684c55f7e2222d7f2e282
GeniusTDY <GeniusTDY@users.noreply.github.com>
2026-07-23 04:17:15 +0000

    Revert commits d55381d..d41ebb2
    This reverts 3 commit(s) from d55381d to d41ebb2.
    
    Reverted commits:
    d41ebb2 feat: 部署CTFd i18n zh-cn-setup-translation 分支
    d42cd99 Revert commits d55381d..d55381d
    d55381d feat: 部署CTFd i18n zh-cn-setup-translation 分支
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- CTFd/themes/admin/assets/js/pages/statistics.js
+++ CTFd/themes/admin/assets/js/pages/statistics.js
@@ -498,6 +498,7 @@ const graph_configs = {
           {
             fillerColor: "rgba(233, 236, 241, 0.4)",
             show: true,
+            right: 60,
             yAxisIndex: 0,
             width: 20,
           },
@@ -613,6 +614,7 @@ const graph_configs = {
           {
             fillerColor: "rgba(233, 236, 241, 0.4)",
             show: true,
+            right: 60,
             yAxisIndex: 0,
             width: 20,
           },
